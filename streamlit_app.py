9227a095e5b1e53f13576c47ffd98b212ab5999c
Sudhakar Damireddy <66399706+sudhakar12161@users.noreply.github.com>
2023-09-21 22:27:25 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit
 
 streamlit.title('Our Health Diet')
 
-streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Bread, Egg, Avacado')
-streamlit.text('Milk with boost')
+streamlit.header('Breakfast Favorites')
+streamlit.text('🐔 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🍞 Bread, Egg, Avacado 🥑')
+streamlit.text('🥣Milk with boost')
