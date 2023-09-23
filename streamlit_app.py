3a7434565558ecdc55d0cbbc79b2b0c9447b7a8a
Sudhakar Damireddy <66399706+sudhakar12161@users.noreply.github.com>
2023-09-23 00:52:55 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,8 @@
 import streamlit
 import requests
 import snowflake.connector
+import pandas as pd
+from urllib.error import URLError
 
 
 streamlit.title('Our Health Diet')
@@ -12,25 +14,46 @@ streamlit.text('🥣Milk with boost')
 
 streamlit.header('Build your own Fruit Smoothie')
 
-import pandas as pd
+
 
 my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list=my_fruit_list.set_index('Fruit')
 fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
-fruit_choice = streamlit.text_input('WHat fruit would you like information about?','Kiwi')
-streamlit.write('The user entered', fruit_choice)
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
-fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
-streamlit.dataframe(fruityvice_normalized)
 
+def get_fruityvise_data(this_fruit_choice):
+  fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+this_fruit_choice)
+  fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
+  return fruityvice_normalized
+  
+try:
+  fruit_choice = streamlit.text_input('WHat fruit would you like information about?')
+  if not fruit_choice:
+    streamlit.error("Please select a fruit to get information.")
+  else:
+    back_from_function = get_fruityvise_data(fruit_choice)
+    streamlit.dataframe(back_from_function)
+except URLError as e:
+  streamlit.error()
+streamlit.stop() 
+streamlit.header("Fruityvise Fruit Advise!")
+try:
+  fruit_entered = streamlit.text_input('What fruit would you like to add?')
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("select * from fruit_load_list")
 my_data_rows = my_cur.fetchall()
-streamlit.header("The fruit load list contains:")
+
 streamlit.dataframe(my_data_rows)
 
-fruit_entered = streamlit.text_input('What fruit would you like to add?')
+
 streamlit.write('Thanks for adding: ',fruit_entered)
+
+my_cur.execute("insert into fruit_load_list values")
+
+
+
+
+
+
