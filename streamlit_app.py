cff86a928cf357cb4388d4c2cd0dc66900545bf7
Sudhakar Damireddy <66399706+sudhakar12161@users.noreply.github.com>
2023-09-22 23:12:00 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,4 +20,5 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/Raspberry")
-streamlit.text(fruityvice_response.json())
+fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
+streamlit.dataframe(fruityvice_normalized)
